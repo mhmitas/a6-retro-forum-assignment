c537aace3b4e62afbb1652663db7a1442a9a6478
Mh Mitas <mahfuzulmitas@gmail.com>
2024-03-03 16:39:13 +0600

    card er news count korechi

--- app.js
+++ app.js
@@ -7,43 +7,64 @@ const loadAllPosts = async () => {
     const allPostsContainer = document.getElementById('all-posts-container')
 
     data.posts.forEach(post => {
-        console.log(post)
+        // console.log(post)
 
         const div = document.createElement('div')
         div.innerHTML = `
         <div class="flex flex-col md:flex-row gap-8 p-8 shadow-xl bg-[#F3F3F5] rounded-xl mb-10">
         <div class="indicator">
-            <span class="indicator-item badge badge-error"></span>
+            <span class="indicator-item badge ${post.isActive ? 'badge-error' : 'badge-success'}"></span>
             <div class="grid w-20 h-20 bg-slate-50 place-items-center rounded-xl">
-                <img class="w-full" src="images/flag.svg" alt="">
+                <img class="w-full rounded-xl" src="${post.image}" alt="">
             </div>
         </div>
-        <div class="space-y-4">
+        <div class="space-y-4 w-full">
             <div class="space-y-2">
-                <div><span class="mr-4"># Music</span> <span>Author : Awlad Hossain</span></div>
-                <h4 class="text-xl font-bold">10 Kids Unaware of Their Halloween Costume</h4>
-                <p>It’s one thing to subject yourself to ha Halloween costume mishap because, hey
-                    that’s
-                    your prerogative</p>
+                <div><span class="mr-4"># ${post.category}</span> <span>Author : ${post?.author?.name}</span></div>
+                <h4 class="text-xl font-bold">${post.title}</h4>
+                <p>${post.description}</p>
             </div>
             <hr>
             <div class="flex justify-between flex-col md:flex-row gap-4">
-                <div class="flex gap-6">
-                    <div class="flex gap-4"><img src="images/message.svg" alt=""><span>550</span>
+                <div class="flex gap-6 items-center">
+                    <div class="flex gap-4"><img src="images/message.svg" alt=""><span>${post.comment_count}</span>
                     </div>|
-                    <div class="flex gap-4"><img src="images/icon-eye.svg" alt=""><span>550</span>
+                    <div class="flex gap-4"><img src="images/icon-eye.svg" alt=""><span>${post.view_count}</span>
                     </div>|
                     <div class="flex gap-4"><img src="images/clock-hour-9.svg"
-                            alt=""><span>550</span>
+                            alt=""><span>${post.posted_time}</span>
                     </div>
                 </div>
-                <img class="w-7" src="images/green-message-icon.png" alt="">
+                <button onclick="readNews('${post.title}', '${post.view_count}')" class="btn bg-indigo-100"><img class="w-7" src="images/green-message-icon.png" alt=""></button>
             </div>
         </div>
     </div>
         `
-    allPostsContainer.appendChild(div)
+        allPostsContainer.appendChild(div)
     });
 }
 
 loadAllPosts()
+
+let newsCount = document.getElementById('news-count').innerText
+const readNews = (title, view) => {
+    // console.log('read done', title, view)
+    const readNewsContainer = document.getElementById('read-news-container')
+
+    const div = document.createElement('div')
+    div.classList= 'flex justify-between p-4 bg-slate-50 rounded-xl'
+    div.innerHTML = `
+    <p>${title}</p>
+    <div class="flex items-center gap-2">
+        <div class="w-8">
+            <img src="images/icon-eye.svg" alt="">
+        </div>
+        <p>${view}</p>
+    </div>
+    `
+    readNewsContainer.appendChild(div)
+
+    newsCount ++
+    document.getElementById('news-count').innerText = newsCount
+    console.log(newsCount)
+}
