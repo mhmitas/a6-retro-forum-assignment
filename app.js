a7c93aef2702ec5115f3e2659104325386934d3a
Mh Mitas <mahfuzulmitas@gmail.com>
2024-03-03 18:09:23 +0600

    some functionality change means change functions

--- app.js
+++ app.js
@@ -1,50 +1,57 @@
 // Load all posts
-const loadAllPosts = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
+const loadAllPosts = async (cat) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${cat}`)
     const data = await res.json()
     // console.log(data.posts)
+    const allPosts = data.posts
+    displayAllPosts(allPosts)
+}
 
+loadAllPosts('')
+
+
+const displayAllPosts = (allPosts) => {
     const allPostsContainer = document.getElementById('all-posts-container')
 
-    data.posts.forEach(post => {
+    allPosts.forEach(post => {
         // console.log(post)
-
         const div = document.createElement('div')
         div.innerHTML = `
-        <div class="flex flex-col md:flex-row gap-8 p-8 shadow-xl bg-[#F3F3F5] rounded-xl mb-10">
-        <div class="indicator">
-            <span class="indicator-item badge ${post.isActive ? 'badge-error' : 'badge-success'}"></span>
-            <div class="grid w-20 h-20 bg-slate-50 place-items-center rounded-xl">
-                <img class="w-full rounded-xl" src="${post.image}" alt="">
-            </div>
-        </div>
-        <div class="space-y-4 w-full">
-            <div class="space-y-2">
-                <div><span class="mr-4"># ${post.category}</span> <span>Author : ${post?.author?.name}</span></div>
-                <h4 class="text-xl font-bold">${post.title}</h4>
-                <p>${post.description}</p>
-            </div>
-            <hr>
-            <div class="flex justify-between flex-col md:flex-row gap-4">
-                <div class="flex gap-6 items-center">
-                    <div class="flex gap-4"><img src="images/message.svg" alt=""><span>${post.comment_count}</span>
-                    </div>|
-                    <div class="flex gap-4"><img src="images/icon-eye.svg" alt=""><span>${post.view_count}</span>
-                    </div>|
-                    <div class="flex gap-4"><img src="images/clock-hour-9.svg"
-                            alt=""><span>${post.posted_time}</span>
-                    </div>
+            <div class="flex flex-col md:flex-row gap-8 p-8 shadow-xl bg-[#F3F3F5] rounded-xl mb-10">
+            <div class="indicator">
+                <span class="indicator-item badge ${post.isActive ? 'badge-error' : 'badge-success'}"></span>
+                <div class="grid w-20 h-20 bg-slate-50 place-items-center rounded-xl">
+                    <img class="w-full rounded-xl" src="${post.image}" alt="">
                 </div>
-                <button onclick="readNews('${post.title}', '${post.view_count}')" class="btn bg-indigo-100"><img class="w-7" src="images/green-message-icon.png" alt=""></button>
             </div>
-        </div>
-    </div>
+            <div class="space-y-4 w-full">
+                <div class="space-y-2">
+                    <div><span class="mr-4"># ${post.category}</span> <span>Author : ${post?.author?.name}</span></div>
+                    <h4 class="text-xl font-bold">${post.title}</h4>
+                    <p>${post.description}</p>
+                </div>
+                <hr>
+                <div class="flex justify-between flex-col md:flex-row gap-4">
+                    <div class="flex gap-6 items-center">
+                        <div class="flex gap-4"><img src="images/message.svg" alt=""><span>${post.comment_count}</span>
+                        </div>|
+                        <div class="flex gap-4"><img src="images/icon-eye.svg" alt=""><span>${post.view_count}</span>
+                        </div>|
+                        <div class="flex gap-4"><img src="images/clock-hour-9.svg"
+                                alt=""><span>${post.posted_time}</span>
+                        </div>
+                    </div>
+                    <button onclick="readNews('${post.title}', '${post.view_count}')" class="btn bg-indigo-100"><img class="w-7" src="images/green-message-icon.png" alt=""></button>
+                </div>
+            </div>
+            </div>
         `
         allPostsContainer.appendChild(div)
     });
 }
 
-loadAllPosts()
+
+
 
 let newsCount = document.getElementById('news-count').innerText
 const readNews = (title, view) => {
@@ -77,10 +84,10 @@ const loadAllLatestPosts = async () => {
 
     const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/latest-posts`)
     const data = await res.json()
-    console.log(data)
+    // console.log(data)
 
     data.forEach(item => {
-        console.log(item)
+        // console.log(item)
 
         const div = document.createElement('div')
         div.classList = 'rounded-xl max-w-[421px] border bg-base-100 shadow-xl p-10 space-y-6'
